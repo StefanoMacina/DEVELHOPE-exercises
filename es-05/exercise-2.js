16e0d46d0b7d763c540ceaebcfb6961302d231ed
Stefano <maclion@live.com>
2023-03-08 16:00:03 +0100

    finito esercizio 1

--- es-05/exercise-2.js
+++ es-05/exercise-2.js
@@ -3,9 +3,31 @@
 // cto => 1800
 // developer => 1500
 // default => 1000
+
+
 function calculateSalary(role) {
-  // ...
+  switch(role) {
+    case "ceo":
+      console.log(role + " " + "=>"+ " " + "€2200");
+      break;
+    case "manager":
+      console.log(role + " " + "=>"+ " " + "€1800");
+      break;
+    case "cto":
+      console.log(role + " " + "=>"+ " " + "€1800");
+      break;
+    case "developer":
+      console.log(role + " " + "=>"+ " " + "€1500");
+      break;
+    default:
+      console.log("€1000");
+      break;
+
+  }
 }
+
+
+
 
 const ceoSalary = calculateSalary('ceo');
 const managerSalary = calculateSalary('manager');
