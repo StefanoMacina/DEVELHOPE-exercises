c70ffd5a406ae72d4777eddbcbeff7f3beaa3f6b
Stefano <maclion@live.com>
2023-03-09 15:56:59 +0100

    Completato esercizio 5

--- es-19/exercise-1.js
+++ es-19/exercise-1.js
@@ -6,7 +6,13 @@ const person1 = {
 
 const person2 = person1;
 
+
+
 // Modify the property `firstName` of the `person2` in "Simon".
+person2.firstName = "Simon"
+
+/*non si è creato un nuovo oggetto "person2" ma si è assegnato
+alla variabile person2 il valore person1*/
 
 console.log(person1);
 console.log(person2);
