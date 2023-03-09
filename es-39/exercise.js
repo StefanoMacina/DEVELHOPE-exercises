5108bcf9a328a07c2f3e1ca82595e63154b77fbd
Stefano <maclion@live.com>
2023-03-09 19:46:11 +0100

    completato esercizio 8

--- es-39/exercise.js
+++ es-39/exercise.js
@@ -1,3 +1,13 @@
 function multiplyByTwo(value) {
   let number = 2;
+    function inner() {
+      result = value * number
+      return result
+  }
+  return inner
 }
+console.log(multiplyByTwo(4)())
+
+
+
+
