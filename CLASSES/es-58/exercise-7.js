b93746d251d10bdaeaebd9bdbbeb720db9be1a7d
Stefano <maclion@live.com>
2023-03-16 10:10:30 +0100

    completato esercizio 4 classi

---
+++ CLASSES/es-58/exercise-7.js
@@ -0,0 +1,34 @@
+class Person {
+  constructor(first, last, age){
+    this.first = first;
+    this.last = last;
+    if(typeof(age) === "number"){
+      this.age = age
+    } else {
+      this.age = Number(age)
+    };
+  }
+
+
+    set first(value){
+      this.firstName = value
+    }
+    set last(value){
+      this.lastName = value
+    }
+
+    set age(value){
+      this.ages = value
+    }
+    get fullName(){
+      return `${this.firstName} ${this.lastName} is ${this.ages} years old`
+    }
+  }
+
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
