58b294bca382b4f5023db084e821171eec522db5
Stefano <maclion@live.com>
2023-04-05 13:45:31 +0200

    completato esercizio 2 callback

---
+++ ASYNCHRONOUS/es-74/exercise.js
@@ -0,0 +1,15 @@
+
+
+
+let sayHello = () =>
+    console.log("Hello")
+
+
+function repeatHello(callback){
+    let id = setInterval(callback, 1000)
+
+
+    setTimeout(() => clearInterval(id),5000)
+}
+
+repeatHello(sayHello)
