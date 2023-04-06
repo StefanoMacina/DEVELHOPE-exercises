52ca72ca341250c26c19e9f82cebd4d312dda2a4
Stefano <maclion@live.com>
2023-04-07 00:44:08 +0200

    added promise chaining

--- LESSONS/promise_chaining/promise.js
+++ LESSONS/promise_chaining/promise.js
@@ -1,1 +1,50 @@
-//
+//------producer code---------------
+// LA RELAZIONE SI BASA SU:
+// getUsername -> getUserDetails -> isUserLoggedIn 
+
+function isLoggedIn(userId){
+    return new Promise((resolve, reject) => {
+        let rand = Math.random()                                        //inserisco id manualmente
+        if(rand > 0.3){                                      //===>     // se il num random è > 0.3 => restituisce lo user ID
+            resolve(userId)
+        } else {
+            reject(new Error("Not logged in"))
+        }
+    })
+}
+
+function getUserDetails(userId){                                        // se lo user id è inferiore a 3 risolve la promise
+    return new Promise((resolve, reject) => {                   //===>  // restituendo un oggetto (simulazione database)
+        if(userId < 5){                                                 // altrimenti a quell'id non è collegato nessun utente
+            resolve({"name": "Greg", "otherData" : "hello"})
+        } else {
+            reject(new Error("User does not exist"))
+        }
+    })
+}
+
+
+function getUsername({name}){
+    return name
+}
+
+//---------------------consumer code-------------------
+//                         CHAINING
+
+//  1) CHIAMARE UNA FUNZIONE
+//  isUserLoggedIn(userId)
+isLoggedIn(3)
+//      2) IN BASE AL SUO OUTPUT CHIAMO LA SECONDA FUNZIONE
+//      .then((userId) => getUserDetails())   
+    .then((val) => getUserDetails(val))
+    //  3) IN BASE AL SUO OUTPUT CHIAMO LA TERZA FUNZIONE
+    //    .then((details) => getUsername(details))
+    .then((val) => getUsername(val))
+    //  4)  ALLA FINE DELLA CATENA CHIAMO IL PARAMETRO "VAL"
+    //  .then((val) => console.log(val))
+    .then((val) => console.log(val))  // (!) val = name
+    //  5)  INFINE GESTISCO GLI ERRORI IN QUALUNQUE ALTRA FUNZIONE
+    //    .catch((err) => console.log(err))
+    .catch((error) => console.log(error.name))
+
+
--- LESSONS/promises/promise.js
+++ LESSONS/promises/promise.js
@@ -1,29 +1,5 @@
 //PROMISE EXPLANATION
 
-// LA RELAZIONE SI BASA SU:
-// getUsername -> getUserDetails -> isUserLoggedIn 
-
-
-//  1) CHIAMARE UNA FUNZIONE
-
-//  isUserLoggedIn(userId)
-
-//  2) IN BASE AL SUO OUTPUT CHIAMO LA SECONDA FUNZIONE   
-
-//    .then((userId) => getUserDetails())
-
-//  3) IN BASE AL SUO OUTPUT CHIAMO LA TERZA FUNZIONE
-
-//    .then((details) => getUsername(details))
-
-//  4)  ALLA FINE DELLA CATENA CHIAMO IL PARAMETRO "NAME"
-
-//  .then((name) => console.log(name))
-
-//  5)  INFINE GESTISCO GLI ERRORI IN QUALUNQUE ALTRA FUNZIONE
-
-//    .catch((err) => console.log(err))
-
 // ESEMPIO PRATICO:
 // creare una promise che si risolve se il numero generato casualmente è maggiore di 0.5, altrimenti restituisce un errore:
 
@@ -32,13 +8,13 @@
     if(rand > 0.5){
       resolve(rand)
     } else {
-      reject(rand)
+      reject(new Error("Whoops"))
     }
 }) */
 
 /* ourPromise
   .then((val) => console.log(val))
-  .catch((err) => console.log(err)) */
+  .catch(error => console.log(error.name)) */
 
 
 // PROMISE CHAINING: STESSO CODICE MA RESTITUIENDO L'OUTPUT PRECEDENTE AL THEN SUCCESSIVO:
@@ -53,9 +29,12 @@ let ourPromise = new Promise((resolve, reject) => {
     }
 })  
 
-ourPromise
+
+//Quando un gestore (handler) ritorna un valore, questo diventa il risultato della promise con il quale sarà chiamato il prossimo .then.
+ourPromise                                                        
   .then((val) => val * 10)       
   .then((val) => `${val} con prima e seconda iterazione`)        // I   valori di output passano ai successivi then fino all'ultimo che stampa il risultato
-  .then((val) => console.log(val))                            // (!) ma solo se la promise viene risolta (rand > 0.5)
+  .then((val) => console.log(val))    //FINE                     // (!) ma solo se la promise viene risolta (rand > 0.5)
   .catch((err) => console.log(err.name))
+
 
