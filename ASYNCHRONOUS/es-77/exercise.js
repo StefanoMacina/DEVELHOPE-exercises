13e0e14e92694da5ec467934b0b90d0e478aae19
Stefano <maclion@live.com>
2023-04-07 02:17:57 +0200

    completato esercizio promise chaining

--- ASYNCHRONOUS/es-77/exercise.js
+++ ASYNCHRONOUS/es-77/exercise.js
@@ -1,1 +1,55 @@
+const isLogged = true;
+
+function getUserStatus(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let rand = Math.random()
+        if(isLogged){
+            resolve(rand)
+        } else {
+            reject(new Error("User does not logged in!"))
+        }
+
+        }, 500)
+    })
+    
+}
+
+
+function getUserData(inputNumber){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(inputNumber > 0.5 && !isNaN(inputNumber) ) {
+                resolve(`{"name": "John", "age": "24"}`)
+            } else {
+                reject(new Error('user does not exist'))
+            }
+        }, 1500)
+        
+    })
+}
+
+
+function fromJson(json){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(JSON.parse(json));
+            reject(new Error("Check the json"))
+        },500)
+        
+    })
+    
+     
+}
+
+
+getUserStatus(isLogged)
+        .then((val) => getUserData(val))
+        .then((val) => fromJson(val))
+        /* .then((val) => JSON.parse(val)) */
+        .then((val) => console.log(val))
+        .catch((error) => console.log(`${error.name} : ${error.type} user`))
+
+
+
 
