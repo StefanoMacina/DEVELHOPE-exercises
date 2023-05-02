3281daf855a8ddcc8be6d8ade87694df71caec34
Stefano <maclion@live.com>
2023-05-02 17:07:44 +0200

    aggiornato esercizio 8.1

--- ex_8/src/index.js
+++ ex_8/src/index.js
@@ -1,44 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Welcome extends React.Component {
-  static defaultProps = {
-    name: "Stefano",
-    age: 24,
-  };
-  render() {
-    return (
-      <>
-        <h1>Welcome {this.props.name}</h1>
-        <Age age={this.props.age}/>
-      </>
-    );
-  }
-}
-
-
-class Age extends React.Component{
-    render(){
-        return(
-            <>
-               <h1>Your age is {this.props.age}</h1> 
-            </>
-        )
-    }
-}
-
-
-class App extends React.Component {
-  render() {
-
-    return (
-      <>
-        <Welcome age={30}/>
-      </>
-    );
-  }
-}
-
+import { App } from "./App";
 
 
 
