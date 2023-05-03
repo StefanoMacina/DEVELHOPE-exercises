7aa074e13da344de0b171343538d2bc9ff35b655
Stefano <maclion@live.com>
2023-05-03 18:26:32 +0200

    completato es 15

---
+++ ex-15/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class Counter extends React.Component {
+  state = {
+    count : this.props.initialValue,
+  }
+
+  constructor(props){
+    super(props)
+    setInterval(() => {
+      
+      this.setState((state) => {
+        return{
+          count : state.count + this.props.increment
+        }
+      })
+    }, this.props.interval);
+
+  }
+
+  render(){
+    return (
+      <h1>{this.state.count}</h1>
+    )
+  }
+}
+
