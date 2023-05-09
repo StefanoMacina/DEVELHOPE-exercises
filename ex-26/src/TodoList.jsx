255f4c7b8475338637a67dc730efc1c1645797a3
Stefano <maclion@live.com>
2023-05-09 12:20:09 +0200

    completato es 26

---
+++ ex-26/src/TodoList.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this._inputRef = React.createRef();
+    this.state = {
+      items: [],
+    };
+  }
+
+  handlestate = () => {
+    const inputText = this._inputRef.current.value;
+    this.setState(() => ({
+      items: [...this.state.items, inputText],
+    }));
+    this._inputRef.current.value = "";
+  };
+
+
+
+  handleReset = () => {
+    this.setState(() => ({
+        items : []
+    }))
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <input ref={this._inputRef} type="text" name="username" />
+        <button onClick={this.handlestate}>CLICK TO ADD</button>
+        <button type="reset" onClick={this.handleReset}>
+          RESET
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
