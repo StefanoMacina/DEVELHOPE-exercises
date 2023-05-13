0744b2e66d90b2d28981c53253eafe26d5e6ebad
Stefano <maclion@live.com>
2023-05-13 15:38:27 +0200

    completato ex 35

---
+++ ex-35/src/Counter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    
+    return ()=> clearInterval(id);
+  }, []);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  );
+}
+
+export default Counter;
