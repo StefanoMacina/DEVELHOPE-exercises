35047b37a5dc21c49b47fe58e76a4ea75f56cec2
Stefano <maclion@live.com>
2023-05-23 17:01:14 +0200

    completato es 51

--- es-51(2)/src/App.jsx
+++ es-51(2)/src/App.jsx
@@ -4,12 +4,15 @@ import Welcome from "./Components/Welcome";
 import Counter from "./Components/Counter";
 import ShowGithubUser from "./Components/ShowGithubUser";
 import NotFound from "./Components/NotFound";
+import GithubUserList from "./Components/GithubUserList";
+import { GithubUserListIndex } from "./Components/GithubUserListIndex";
 
 
 function App() {
   return (
     <Router>
-      <nav>
+      <div>
+        <nav>
           <ul>
             <li>
               <Link to="/">Welcome</Link>
@@ -18,18 +21,21 @@ function App() {
               <Link to="/counter">Counter</Link>
             </li>
             <li>
-              <Link to="/users/StefanoMAcina">User</Link>
+              <Link to="/users">User</Link>
             </li>
           </ul>
         </nav>
-      <Routes>
-        <Route path="/" element={<Welcome name="Stefano" />}></Route>
-        <Route path="/Counter" element={<Counter />}></Route>
-        <Route path="/users" element={<Outlet/>} >
-          <Route path=":username" element={<ShowGithubUser />}></Route>
-        </Route>
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+
+        <Routes>
+          <Route path="/" element={<Welcome name="Stefano" />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users" element={<GithubUserList />}>
+            <Route index element={<GithubUserListIndex />} />
+            <Route path=":username" element={<ShowGithubUser />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ es-51(2)/src/Components/GithubUserListIndex.jsx
@@ -0,0 +1,3 @@
+export function GithubUserListIndex() {
+    return <h3>Add a user and select it</h3>;
+  }
