022a19b2709f82ed200e89a198bee993e7a51b1c
Stefano <maclion@live.com>
2023-05-30 11:18:09 +0200

    add

--- es-50/src/GItListUser.jsx
+++ es-50/src/GItListUser.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 
 export function GithubUserList() {
-  const usernames = ["Stefano", "Antonio", "Paolo"];
+  const usernames = ["Stefano", "Antonio", "Paolo", 'nome'];
 
   return (
     <div>
