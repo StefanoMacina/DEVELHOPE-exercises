9998a4546480a1515b03ce9fe209766fcb37af07
Stefano <maclion@live.com>
2023-06-06 11:01:22 +0200

    completato es 8

---
+++ ex-8/promise.mjs
@@ -0,0 +1,35 @@
+
+
+
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+
+luckyDraw('joe')
+    .then((result) => {
+        console.log(result)
+        return luckyDraw('Carolina')
+    })
+    .then((result2) => {
+        console.log(result2)
+        return luckyDraw('Sabrina')
+    })
+    .then((result3) => {
+        console.log(result3)
+        
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
