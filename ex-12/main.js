9cfb1d5aea180a904c4627a5d50b8e81c48f633b
Stefano <maclion@live.com>
2023-06-15 17:18:32 +0200

    updated : ex 12

--- ex-12/main.js
+++ ex-12/main.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const {getAll , getOneById , create , updateById , deleteById} = require('./planets')
+const express = require("express");
+const app = express();
+const port = 4000;
+const {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} = require("./planets");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/planets' , getAll)
+app.get("/api/planets", getAll);
 
-app.get('/api/planets/:id' , getOneById)
+app.get("/api/planets/:id", getOneById);
 
-app.post('/api/planets' , create)
+app.post("/api/planets", create);
 
-app.put('/api/planets/:id' , updateById)
+app.put("/api/planets/:id", updateById);
 
-app.delete('/api/planets/:id' , deleteById)
-
-
-
+app.delete("/api/planets/:id", deleteById);
 
 app.listen(port, () => {
-    console.log(`server listening at http://localhost:${port}`)
-})
+  console.log(`server listening at http://localhost:${port}`);
+});
--- ex-12/planets.js
+++ ex-12/planets.js
@@ -1,59 +1,63 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 const planetSchema = Joi.object({
-    id : Joi.number().integer().required(),
-    name : Joi.string().required()
-})
+  id: Joi.number().integer().required(),
+  name: Joi.string().required(),
+});
 
 let planets = [
-    {id : 1 , name : 'Earth'},
-    {id : 2 , name : 'Mars'}
-]
+  { id: 1, name: "Earth" },
+  { id: 2, name: "Mars" },
+];
 
-const getAll = (req , res) => {
-    res.status(200).json({msg : 'success' , data : planets})
-}
+const getAll = (req, res) => {
+  res.status(200).json({ msg: "success", data: planets });
+};
 
-const getOneById = (req , res) => {
-    const { id } = req.params
-    const planet = planets.find((p) => p.id === Number(id))
-    res.status(200).json({msg : 'success' , data : planet})
-}
+const getOneById = (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json({ msg: "success", data: planet });
+};
 
 const create = (req, res) => {
-    const newPlanet = req.body
-    const validateNewPlanet = planetSchema.validate(newPlanet)
-    if(validateNewPlanet.error){
-        res.status(400).json({msg : validateNewPlanet.error.details[0].message})
-    } else {
-        planets = [...planets , newPlanet]
-        res.status(200).json({msg : 'success'})
-    }
-    
-}
+  const newPlanet = req.body;
+  const validateNewPlanet = planetSchema.validate(newPlanet);
+  if (validateNewPlanet.error) {
+    res.status(400).json({ msg: validateNewPlanet.error.details[0].message });
+  } else {
+    planets = [...planets, newPlanet];
+    res.status(200).json({ msg: "success" });
+  }
+};
 
 const namePlanetSchema = Joi.object({
-    name : Joi.string().required()
-})
+  name: Joi.string().required(),
+});
 
 const updateById = (req, res) => {
-    const {id} = req.params
-    const {name} = req.body
-    planets = planets.map((p) => (p.id === Number(id) ? {...p,name} : p))
-    const validateUpdatedPlanet = namePlanetSchema.validate(planets)
-    if(validateUpdatedPlanet.error){
-        res.status(400).json({ msg : validateUpdatedPlanet.error})
-    } else {
-        res.status(200).json({msg : 'updated'})
-    }
-}
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+  const validateUpdatedPlanet = namePlanetSchema.validate(planets);
+  if (validateUpdatedPlanet.error) {
+    res.status(400).json({ msg: validateUpdatedPlanet.error });
+  } else {
+    res.status(200).json({ msg: "updated" });
+  }
+};
 
+const deleteById = (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+  res.status(200).json({ msg: "deleted" });
+};
 
-const deleteById = (req , res) => {
-    const {id} = req.params
-    planets = planets.filter((p) => p.id !== Number(id))
-    res.status(200).json({msg : 'deleted'})
-}
-
-
-module.exports = {planets , getAll  , getOneById , create , updateById , deleteById}
+module.exports = {
+  planets,
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+};
