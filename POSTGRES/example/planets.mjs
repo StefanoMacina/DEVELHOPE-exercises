fc21a82c914e590a20ddeac68b9018a43621fd82
Stefano <maclion@live.com>
2023-06-27 18:03:47 +0200

    add: other

--- POSTGRES/example/planets.mjs
+++ POSTGRES/example/planets.mjs
@@ -1,24 +1,26 @@
 import pgPromise from "pg-promise";
 
 // CREAZIONE COLLEGAMENTO A POSTGRESS DB
-const db = pgPromise()("postgres://postgres:postgres@localhost:5432/postgres");
+const db = pgPromise()("postgres://postgres:appleinc1@localhost:5432/planets");
 
 // SETUP DB CON CREAZIONE DI UNA TABELLA
 const setupDb = async () => {
-    
+
   await db.none(`
-    DROP TABLE IF EXIST planets;
+
+    DROP TABLE IF EXISTS planets;
 
     CREATE TABLE planets (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
     )
+    
 `);
 
-    await db.none(`INSERT INTO planets (name) VALUE ('Earth')`)
-    await db.none(`INSERT INTO planets (name) VALUE ('Mars')`)
+    await db.none(`INSERT INTO planets (name) VALUES ('Earth')`)
+    await db.none(`INSERT INTO planets (name) VALUES ('Mars')`)
 
-    const planets = await db.many(`SELECT * FORM planets`)
+    const planets = await db.many(`SELECT * FROM planets`)
     console.log(planets);
 };
 
