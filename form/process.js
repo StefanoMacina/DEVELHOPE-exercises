9b9b3efbb211e0e946303342c22c612c9d4d550d
Stefano <maclion@live.com>
2023-06-27 18:21:05 +0200

    add

--- form/process.js
+++ form/process.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.use(express.static('public'))
 app.use(express.json())
+
 
 app.get('/info', (req , res ) => {
     res.status(200).json({info : 'preset text'})
 })
+
 
 app.post('/' , (req , res) => {
     const { parcel } = req.body
     console.log(parcel);
     res.status(200).send( { status : 'received' })
 })
+
 
 app.listen(port , () => {
     console.log(`server is running at http://localhost:${port}`)
